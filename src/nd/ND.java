1b4b1f0eda434939aced861b7bff0e6ce0356852
Vadim <vadim.mozeiko@gmail.com>
2021-03-11 10:01:27 +0200

    screenshot

--- src/nd/ND.java
+++ src/nd/ND.java
@@ -7,44 +7,6 @@ public class ND {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-//        System.out.println("Ivesti pirma skaiciu:");
-//        int num1 = sc.nextInt();
-//        System.out.println("Ivesti antra skaiciu:");
-//        int num2 = sc.nextInt();
-//        System.out.println("Ivesti trecia skaiciu:");
-//        int num3 = sc.nextInt();
-//        System.out.println("Ivesti ketvirta skaiciu:");
-//        int num4 = sc.nextInt();
-//
-//        System.out.println("-----------------------");
-//        float average = (num1 + num2 + num3 + num4) / 4f;
-//        System.out.println("Ivestu skaiciu vidurkis:" + average);
-//
-//        int firstNumSq = num1 * num1;
-//        int secondNumSq = num2 * num2;
-//        int thirdNumSq = num3 * num3;
-//        int fourthNumSq = num4 * num4;
-//        System.out.println("-----------------------");
-//        System.out.println("Pirmo skaiciaus kvadratas:" + firstNumSq);
-//        System.out.println("Antro skaiciaus kvardatas:" + secondNumSq);
-//        System.out.println("Trecio skaiciaus kvadratas:" + thirdNumSq);
-//        System.out.println("Ketvirto skaiciaus kvadratas:" + fourthNumSq);
-//
-//        int firstNumCb = num1 * num1 * num1;
-//        int secondNumCb = num2 * num2 * num2;
-//        int thirdNumCb = num3 * num3 * num3;
-//        int fourthNumCb = num4 * num4 * num4;
-//        System.out.println("-----------------------");
-//        System.out.println("Pirmo skaiciaus kubas:" + firstNumCb);
-//        System.out.println("Antro skaiciaus kubas:" + secondNumCb);
-//        System.out.println("Trecio skaiciaus kubas:" + thirdNumCb);
-//        System.out.println("Ketvirto skaiciaus kubas:" + fourthNumCb);
-//
-//        float averageCb = (firstNumCb + secondNumCb + thirdNumCb + fourthNumCb) / 4f;
-//        
-//        System.out.println("-----------------------");
-//        System.out.println("Ivestu skaiciu kubu vidurkis:" + averageCb );
         System.out.println("Amount of number you want to enter:");
         System.out.println();
         int quantity = sc.nextInt();
